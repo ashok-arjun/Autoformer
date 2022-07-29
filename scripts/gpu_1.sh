f7df3e0da6e95055338e5882a7bd98950081b816
Arjun Ashok <arjun2000ashok@gmail.com>
2022-07-29 14:52:51 +0530

    Update gpu_1.sh

--- scripts/gpu_1.sh
+++ scripts/gpu_1.sh
@@ -1,37 +1,40 @@
 export CUDA_VISIBLE_DEVICES=0
 
-for PRED_LEN in 24
+for FACTOR in 3 2 1 4
 do
-  for E_LAYERS in 1
+  for SEQ_LEN in 96 192
   do
-    for D_LAYERS in 1 2 3
+    for E_LAYERS in 2 1
     do
-      for FACTOR in 1 2 3
+      for D_LAYERS in 3 2 1
       do
-        python -u run.py \
-          --is_training 1 \
-          --root_path ./dataset/windspeed/ \
-          --data_path bahia_windspeed.csv \
-          --model_id windspeed_96_24 \
-          --model Autoformer \
-          --data Windspeed \
-          --features 'MS' \
-          --seq_len 96 \
-          --target 'windspeed' \
-          --label_len 48 \
-          --pred_len $PRED_LEN \
-          --e_layers $E_LAYERS \
-          --d_layers $D_LAYERS \
-          --factor $FACTOR \
-          --enc_in 9 \
-          --dec_in 9 \
-          --c_out 1 \
-          --des "Exp_Autoformer_windspeed_96_${PRED_LEN}_Factor_${FACTOR}_ELayers_${E_LAYERS}_DLayers_${D_LAYERS}" \
-          --itr 1 \
-          --tags "to-check-convergence" \
-          --train_epochs 40 \
-          --learning_rate 1e-4 \
-          --patience 5
+        for PRED_LEN in 24 48 96 192 336 720
+        do
+          python -u run.py \
+            --is_training 1 \
+            --root_path ./dataset/beijing/ \
+            --data_path Changping_PM2.5.csv \
+            --model_id "Autoformer_Seq_${SEQ_LEN}_Pred_${PRED_LEN}_Factor_${FACTOR}_ELayers_${E_LAYERS}_DLayers_${D_LAYERS}" \
+            --model Autoformer \
+            --data custom \
+            --features 'MS' \
+            --seq_len $SEQ_LEN \
+            --target 'PM2.5' \
+            --label_len 48 \
+            --pred_len $PRED_LEN \
+            --e_layers $E_LAYERS \
+            --d_layers $D_LAYERS \
+            --factor $FACTOR \
+            --enc_in 6 \
+            --dec_in 6 \
+            --c_out 1 \
+            --des "Autoformer_Seq_${SEQ_LEN}_Pred_${PRED_LEN}_Factor_${FACTOR}_ELayers_${E_LAYERS}_DLayers_${D_LAYERS}" \
+            --itr 1 \
+            --tags "paper" \
+            --train_epochs 100 \
+            --learning_rate 1e-4 \
+            --patience 5
+        done
       done
     done
   done
